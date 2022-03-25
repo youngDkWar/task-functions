cb50fecfa96afdb8deb79fbbb6383387beed2932
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-03-25 14:53:01 +0500

    task-functions

--- src/Functions.js
+++ src/Functions.js
@@ -1,12 +1,20 @@
 /*
 Напишите коллбек для array.filter, который отбирает все числа больше num
  */
-export function higherThan(num) {}
+export function higherThan(num) {
+    return function (e) {
+        return e > num;
+    };
+}
 
 /*
 Напишите коллбек для array.filter, который отбирает из массива все строки, включающие в себя substr. Регистр важен.
  */
-export function hasSubstring(substr) {}
+export function hasSubstring(substr) {
+    return function (e) {
+        return e.indexOf(substr) > -1 ? true : false;
+    };
+}
 
 /*
 Напишите функцию `multiply(num)`, которая работает вот так:
@@ -14,4 +22,8 @@ export function hasSubstring(substr) {}
 console.log(multiply(5)(10));
 // 50
  */
-export function multiply(num) {}
+export function multiply(num1) {
+    return function (num2) {
+        return num1 * num2;
+    };
+}
